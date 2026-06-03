8f3f71731803d7fd737fbea7ea9644c4e95ea4d9
TheRealRemo <82535413+TheRealRemo@users.noreply.github.com>
2026-06-03 10:35:15 -0400

    Add files via upload
    answered all questions

--- joins.sql
+++ joins.sql
@@ -15,4 +15,22 @@ ORDER BY ProductName;
 SELECT ProductID, ProductName, UnitPrice, CategoryName, CompanyName
 FROM products
 JOIN categories ON products.CategoryID = categories.CategoryID
-JOIN suppliers ON products.SupplierID = suppliers.SupplierID; 
+JOIN suppliers ON products.SupplierID = suppliers.SupplierID
+Order BY ProductName;
+
+-- Question 4
+SELECT ProductName, CategoryName, UnitPrice
+From products
+Join categories ON products.CategoryID = categories.CategoryID
+WHERE UnitPrice = (SELECT MAX(UnitPrice) FROM products); 
+-- Question 5
+SELECT OrderID, ShipName, ShipAddress, CompanyName, ShipCountry
+From orders 
+Join shippers ON orders.ShipVia = shippers.ShipperID
+WHERE ShipCountry = "Germany";
+-- Question 6
+SELECT o.OrderID, OrderDate, ShipName, ShipAddress, ProductName
+From orders o
+Join `Order Details` ON o.OrderID = `Order Details`.OrderID
+Join products ON `Order Details`.ProductID = products.ProductID
+WHERE ProductName = "Sasquatch Ale";
